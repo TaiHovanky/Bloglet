f6679891621806494a1241d88460721ccfbcdb34
Tai Hovanky <tai.hovanky@gmail.com>
2022-01-20 10:52:35 -0600

    using docker host internal

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -9,7 +9,7 @@ import './index.css';
 import cache from './cache';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: 'http://host.docker.internal:3001/graphql',
   credentials: 'include' // need this so that cookie gets set after login response,
 });
 
