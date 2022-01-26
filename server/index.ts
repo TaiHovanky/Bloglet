890d4988dd78c9fbeeff497aa3ecdec6650d32bc
Tai Hovanky <tai.hovanky@gmail.com>
2022-01-25 18:42:10 -0600

    port

--- server/index.ts
+++ server/index.ts
@@ -18,7 +18,7 @@ import { createAccessToken, createRefreshToken } from './src/utils/create-tokens
 (async () => {
   const app = express();
   app.use(cors({
-    origin: 'http://159.223.122.194:3000',
+    origin: 'http://159.223.122.194:80',
     credentials: true
   }));
   app.use(cookieParser());
