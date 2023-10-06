64b5a147335e2a942571ecd67187b85b59a3fbe0
Tai Hovanky <tai.hovanky@gmail.com>
2023-10-06 15:52:24 -0500

    test 10

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,5 +1,9 @@
-console.log('process.env.DB_HOST', process.env.DB_HOST, process.env.DB_PORT, process.env.DB_USER, process.env.DB_PW, process.env.DB_NAME);
+import {createConnection} from "typeorm";
 
+console.log('process.env.DB_HOST', process.env.DB_HOST, process.env.DB_PORT, process.env.DB_USER, process.env.DB_PW, process.env.DB_NAME);
+// createConnection method will automatically read connection options
+// from your ormconfig file or environment variables
+export const connection = await createConnection();
 module.exports = {
    "type": "postgres",
    "host": process.env.DB_HOST,
